a75f31d00268f53069d94b9a1a5793c68ba010bc
AntonyIS <antonyshikubu@gmail.com>
2019-11-01 19:51:00 +0300

    Second commit

--- login.php
+++ login.php
@@ -8,6 +8,44 @@ if(isset($_GET['success_msg'])){
         echo  $_GET['success_msg'];
     echo "</div>";
 }
+
+$email  = $password ='';
+if(isset($_POST['loginpBtn'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+//    hash the password
+    $password = md5($password);
+//    chec if theres a record with the same email and password
+//    if true login the user, take them to index page
+        $sql = "SELECT `id`, `username`, `email`, `password` FROM `users` WHERE email='$email' AND password='$password'";
+      $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+           $id = $row['id'];
+           session_start();
+           $_SESSION['kipande'] = $id;
+           $_SESSION['loggedin'] = true;
+//           $kipande = $id
+            $success = "Login is successful, Have fun";
+            header('location:index.php?login_success='.$success);
+        }
+    } else {
+        //    if false  take user to login page and ask them to try again
+//       $error = "Login unsuccessful, Please try again";
+//       header('location:login.php?login_err='.$error);
+        echo "Error ".$sql .mysqli_error($conn);
+    }
+
+
+
+}
+
+
+
+
 ?>
 
 
